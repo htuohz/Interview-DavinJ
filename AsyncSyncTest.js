524f8b8401d0f4c9602c495b846adf44018a44ae
Jinhao Jiang <58126838+DavinJ-0316@users.noreply.github.com>
2022-06-29 11:50:03 +1000

    Add files via upload

---
+++ AsyncSyncTest.js
@@ -0,0 +1,30 @@
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var request = new XMLHttpRequest();
+request.open(
+  "GET",
+  "http://192.168.1.23:9984/api/v1/metadata?search=dev77",
+  false
+); // `false` makes the request synchronous
+request.send(null);
+
+if (request.status === 200) {
+  console.log(request.responseText);
+  console.log("data");
+}
+
+// const axios = require("axios").default;
+// const tester = async () => {
+//   try {
+//     const { data } = await axios.get(
+//       "http://localhost:9984/api/v1/metadata?search=dev77"
+//     );
+//     console.log(data);
+//     console.log("data");
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+// tester();
+
+console.log("ok");
